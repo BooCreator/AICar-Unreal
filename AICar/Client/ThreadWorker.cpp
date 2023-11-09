523455c41bc0edc866eb1aca183242ddeeb5b913
BooCreator <boocreator.official@gmail.com>
2023-11-09 15:11:41 +0200

    Update ThreadWorker.cpp

--- AICar/Client/ThreadWorker.cpp
+++ AICar/Client/ThreadWorker.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Client/ThreadWorker.h"
+#include "ThreadWorker.h"
 
 
 FThreadWorker* FThreadWorker::Runnable = NULL;
