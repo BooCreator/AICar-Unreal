81d05a49a7507ec3e743708a4f2b928da28c92ef
BooCreator <boocreator.official@gmail.com>
2023-11-09 10:53:49 +0200

    Update WebSockets.cpp

--- AICar/Client/WebSockets.cpp
+++ AICar/Client/WebSockets.cpp
@@ -9,40 +9,42 @@ WebSockets::WebSockets() {
 	}
 }
 
-WebSockets::~WebSockets() { this->Close(); }
+WebSockets::~WebSockets() { 
+	//this->Close(); 
+}
 
 void WebSockets::Init(FString server, int ai_count) {
-	this->Close();
-	WebSockets_run = true;
-	this->ServerURL = server;
+	//this->Close();
+	//WebSockets_run = true;
+	//this->ServerURL = server;
 	//if (ai_count > 0)
 	//	this->connection = new std::thread(&WebSockets::doConnect);
 }
 
 void WebSockets::doConnect() {
-	TSharedPtr<IWebSocket> Socket = FWebSocketsModule::Get().CreateWebSocket(this->ServerURL, this->ws);
-
-	Socket->OnConnected().AddRaw(this, &WebSockets::OnConnected);
-	Socket->OnConnectionError().AddRaw(this, &WebSockets::OnError);
-	Socket->OnMessage().AddRaw(this, &WebSockets::OnMessage);
-
-	while (WebSockets_run) {
-		int sleep_time = 1;
-		if (Socket->IsConnected()) {
-			if (WebSockets_Requests.size() > 0) {
-				FString JSONPayload = WebSockets_Requests.front();
-				WebSockets_Requests.pop();
-				Socket->Send(JSONPayload);
-			}
-		}
-		else {
-			UE_LOG(LogTemp, Warning, TEXT("WebSockets is not connected! Connecting..."));
-			Socket->Connect();
-			sleep_time = 1000;
-		}
-		Sleep(sleep_time);
-	}
-	Socket->Close();
+	//TSharedPtr<IWebSocket> Socket = FWebSocketsModule::Get().CreateWebSocket(this->ServerURL, this->ws);
+//
+	//Socket->OnConnected().AddRaw(this, &WebSockets::OnConnected);
+	//Socket->OnConnectionError().AddRaw(this, &WebSockets::OnError);
+	//Socket->OnMessage().AddRaw(this, &WebSockets::OnMessage);
+//
+	//while (WebSockets_run) {
+	//	int sleep_time = 1;
+	//	if (Socket->IsConnected()) {
+	//		if (WebSockets_Requests.size() > 0) {
+	//			FString JSONPayload = WebSockets_Requests.front();
+	//			WebSockets_Requests.pop();
+	//			Socket->Send(JSONPayload);
+	//		}
+	//	}
+	//	else {
+	//		UE_LOG(LogTemp, Warning, TEXT("WebSockets is not connected! Connecting..."));
+	//		Socket->Connect();
+	//		sleep_time = 1000;
+	//	}
+	//	Sleep(sleep_time);
+	//}
+	//Socket->Close();
 }
 
 void WebSockets::Close() {
@@ -63,5 +65,5 @@ void WebSockets::OnError(const FString & Message) {
 
 void WebSockets::OnMessage(const FString & Message) {
 	UE_LOG(LogTemp, Warning, TEXT("WebSockets on_message: %s"), *Message);
-	WebSockets_Messages.push(Message);
+	//WebSockets_Messages.push(Message);
 }
